f96e725ff2026b23c0bbb079b8e15054923b0f83
Dussart Guillaume <dussartguillaume@orange.fr>
2017-09-13 13:12:29 +0200

    minichat.php->add form

---
+++ minichat.php
@@ -0,0 +1,11 @@
+<form>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Pseudo</label>
+        <input type="text" name="pseudo" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Content</label>
+        <textarea class="form-control" name="content" id="exampleInputPassword1"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
